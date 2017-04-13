f990e94964ed323559ae3aea491a9a7ed671ef47
jonschlinkert <jon.schlinkert@sellside.com>
2017-04-12 23:30:35 -0400

    update deps, reorganize code

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 /*!
  * write-data <https://github.com/jonschlinkert/write-data>
  *
- * Copyright (c) 2015, Jon Schlinkert.
- * Licensed under the MIT License.
+ * Copyright (c) 2015, 2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
 
 var path = require('path');
-var utils = require('./utils');
+var json = require('write-json');
+var yaml = require('write-yaml');
+var extend = require('extend-shallow');
+var isObject = require('isobject');
 
 /**
  * Asynchronously write JSON or YAML to disk, creating any
@@ -17,49 +20,51 @@ var utils = require('./utils');
  *
  * ```js
  * writeData('foo.yml', {foo: "bar"}, function(err) {
- *   if (err) return console.log(err);
+ *   // do stuff with err
  * });
  * ```
- * @param  {String} `dest` Destination filepath.
- * @param  {Object} `data` The data object to write.
- * @param  {Options} `options`
- * @param  {Function} `cb` Callback function
- * @api public
+ * @param  {String} `dest` (required) Destination filepath.
+ * @param  {Object} `data` (required) The data object to write.
+ * @param  {Options} `options` (optional)
+ * @param  {Function} `cb` (required) Callback function
  */
 
 module.exports = function(dest, data, options, cb) {
   if (typeof options === 'function') {
     cb = options;
-    options = {};
+    options = undefined;
   }
 
   if (typeof cb !== 'function') {
-    throw new TypeError('write-data async expects a callback function.');
+    throw new TypeError('expected callback to be a function');
   }
   if (typeof dest !== 'string') {
-    return cb(new TypeError('write-data async expects dest to be a string.'));
+    cb(new TypeError('expected dest path to be a string'));
+    return;
   }
-  if (typeof data !== 'object') {
-    return cb(new TypeError('write-data async expects data to be an object.'));
+  if (!isObject(data)) {
+    cb(new TypeError('expected data to be an object'));
+    return;
   }
 
-  var defaults = {ext: path.extname(dest), indent: 2};
-  var opts = utils.extend({}, defaults, options);
+  var opts = extend({indent: 2, safe: true}, options);
+  var ext = opts.ext || path.extname(dest);
 
-  var ext = opts.ext;
-  if (ext.charAt(0) === '.') {
-    ext = ext.slice(1);
+  if (ext.charAt(0) !== '.') {
+    ext = '.' + ext;
   }
 
   switch (ext) {
-    case 'json':
-      return utils.writeJson(dest, data, cb);
-    case 'yml':
-    case 'yaml':
-      return utils.writeYaml(dest, data, opts, cb);
+    case '.yml':
+    case '.yaml':
+      yaml(dest, data, opts, cb);
+      break;
+    case '.json':
+      json(dest, data, cb);
+      break;
     default: {
-      cb(new Error('writeData does not support file extension: ' + ext));
-      return;
+      cb(new Error('unsupported file extension: ' + ext));
+      break;
     }
   }
 };
@@ -72,41 +77,34 @@ module.exports = function(dest, data, options, cb) {
  * ```js
  * writeData.sync('foo.yml', {foo: "bar"});
  * ```
- *
- * @param  {String} `dest` Destination filepath.
- * @param  {Object} `data` The data object to write.
- * @param  {Options} `options`
- * @api public
+ * @param  {String} `dest` (required) Destination filepath.
+ * @param  {Object} `data` (required) The data object to write.
+ * @param  {Options} `options` (optional)
  */
 
 module.exports.sync = function(dest, data, options) {
   if (typeof dest !== 'string') {
-    throw new TypeError('write-data expects dest path to be a string.');
+    throw new TypeError('expected dest path to be a string');
   }
-  if (typeof data !== 'object') {
-    throw new TypeError('write-data expects data to be an object.');
-  }
-
-  var defaults = {ext: path.extname(dest), indent: 2};
-  var opts = utils.extend({}, defaults, options);
-
-  var ext = opts.ext;
-  if (ext.charAt(0) === '.') {
-    ext = ext.slice(1);
+  if (!isObject(data)) {
+    throw new TypeError('expected data to be an object');
   }
 
-  var writer = utils.writeJson.sync;
+  var opts = extend({indent: 2, safe: true}, options);
+  var ext = opts.ext || path.extname(dest);
+
+  if (ext.charAt(0) !== '.') {
+    ext = '.' + ext;
+  }
+
   switch (ext) {
-    case 'json':
-      writer = utils.writeJson.sync;
-      break;
-    case 'yml':
-    case 'yaml':
-      writer = utils.writeYaml.sync;
-      break;
+    case '.yml':
+    case '.yaml':
+      return yaml.sync(dest, data, opts);
+    case '.json':
+      return json.sync(dest, data);
     default: {
-      throw new Error('writeDataSync does not support file extension: ' + ext);
+      throw new Error('unsupported file extension: ' + ext);
     }
   }
-  return writer(dest, data, opts);
 };
--- test/test.js
+++ test/test.js
@@ -8,7 +8,6 @@
 'use strict';
 
 require('mocha');
-require('should');
 var fs = require('fs');
 var path = require('path');
 var assert = require('assert');
@@ -18,62 +17,25 @@ var del = require('delete');
 
 var files = ['tmp/a.md', 'tmp/b.md', 'tmp/c.md', 'tmp/d.md', 'tmp/e.md'];
 
-describe('sync', function () {
-  it('should write a yaml file synchronously', function () {
-    writeData.sync('test/actual/a.yml', {a: {b: {c: {d: 'e'}}}});
-    var json = readYaml.sync('test/actual/a.yml');
-    json.a.should.have.property('b');
-    json.a.b.should.have.property('c');
-  });
-
-  it('should use specified indentation.', function () {
-    writeData.sync('test/actual/b.yml', {a: {b: {c: {d: 'e'}}}}, {indent: 4});
-    var str = fs.readFileSync('test/actual/b.yml', 'utf8');
-    var secondLine = str.split('\n')[1];
-    var indent = secondLine.match(/^(\s+)/)[0].split('').length;
-    indent.should.equal(4);
-  });
-});
-
-describe('async', function () {
-  it('should write a yaml file asynchronously', function (cb) {
-    writeData('test/actual/a.yml', {a: {b: {c: {d: 'e'}}}}, function (err) {
-      if (err) {
-        console.log(err);
-        return cb(err);
-      }
-      var json = readYaml.sync('test/actual/a.yml');
-      json.a.should.have.property('b');
-      json.a.b.should.have.property('c');
-      cb();
-    });
-  });
-
-  it('should use specified indentation.', function (cb) {
-    writeData('test/actual/b.yml', {a: {b: {c: {d: 'e'}}}}, {indent: 4}, function (err) {
-      if (err) {
-        console.log(err);
-        return cb(err);
-      }
-      var str = fs.readFileSync('test/actual/b.yml', 'utf8');
-      var secondLine = str.split('\n')[1];
-      var indent = secondLine.match(/^(\s+)/)[0].split('').length;
-      indent.should.equal(4);
-      cb();
-    });
-  });
-});
-
-describe('write-json', function () {
-  afterEach(function (cb) {
+describe('write-data', function() {
+  afterEach(function(cb) {
     del('actual', cb);
   });
 
-  describe('async', function () {
-    it('should write JSON asyncronously', function (cb) {
+  describe('json / async', function() {
+    it('should throw on invalid extensions', function(cb) {
       var fixture = {foo: {bar: "baz"} };
-      writeData('actual/test.json', fixture, function (err) {
-        fs.readFile('actual/test.json', 'utf8', function (err, res) {
+      writeData('actual/test.txt', fixture, function(err) {
+        assert(err);
+        assert(/unsupported/.test(err.message));
+        cb();
+      });
+    });
+
+    it('should write JSON asyncronously', function(cb) {
+      var fixture = {foo: {bar: "baz"} };
+      writeData('actual/test.json', fixture, function(err) {
+        fs.readFile('actual/test.json', 'utf8', function(err, res) {
           if (err) return cb(err);
           assert.deepEqual(JSON.parse(res), fixture);
           cb();
@@ -82,13 +44,66 @@ describe('write-json', function () {
     });
   });
 
-  describe('sync', function () {
-    it('should write JSON syncronously', function (cb) {
+  describe('yaml / async', function() {
+    it('should write a yaml file asynchronously', function(cb) {
+      var data = {a: {b: {c: {d: 'e'}}}};
+      writeData('test/actual/a.yml', data, function(err) {
+        if (err) {
+          cb(err);
+          return;
+        }
+        var json = readYaml.sync('test/actual/a.yml');
+        assert(json.a.hasOwnProperty('b'));
+        assert(json.a.b.hasOwnProperty('c'));
+        cb();
+      });
+    });
+
+    it('should use specified indentation.', function(cb) {
+      var data = {a: {b: {c: {d: 'e'}}}};
+      writeData('test/actual/b.yml', data, {indent: 4}, function(err) {
+        if (err) {
+          cb(err);
+          return;
+        }
+        var str = fs.readFileSync('test/actual/b.yml', 'utf8');
+        var secondLine = str.split('\n')[1];
+        var indent = secondLine.match(/^(\s+)/)[0].split('').length;
+        assert.equal(indent, 4);
+        cb();
+      });
+    });
+  });
+
+  describe('json / sync', function() {
+    it('should throw on invalid extensions', function() {
+      assert.throws(function() {
+        writeData.sync('foo.txt', {});
+      }, /unsupported/);
+    });
+
+    it('should write JSON syncronously', function() {
       var fixture = {foo: {bar: "baz"} };
       writeData.sync('actual/test.json', fixture);
       var res = fs.readFileSync('actual/test.json', 'utf8');
       assert.deepEqual(JSON.parse(res), fixture);
-      cb();
+    });
+  });
+
+  describe('yaml / sync', function() {
+    it('should write a yaml file synchronously', function() {
+      writeData.sync('test/actual/a.yml', {a: {b: {c: {d: 'e'}}}});
+      var json = readYaml.sync('test/actual/a.yml');
+      assert(json.a.hasOwnProperty('b'));
+      assert(json.a.b.hasOwnProperty('c'));
+    });
+
+    it('should use specified indentation.', function() {
+      writeData.sync('test/actual/b.yml', {a: {b: {c: {d: 'e'}}}}, {indent: 4});
+      var str = fs.readFileSync('test/actual/b.yml', 'utf8');
+      var secondLine = str.split('\n')[1];
+      var indent = secondLine.match(/^(\s+)/)[0].split('').length;
+      assert.equal(indent, 4);
     });
   });
 });
